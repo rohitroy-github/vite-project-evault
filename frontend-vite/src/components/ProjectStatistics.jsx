1a8017d06b64d983289cf3dea13d061775a7d0d6
Aritra Mondal <sasuke.aritra.sa@gmail.com>
2024-03-30 22:17:13 +0530

    charts addition in  project statistics in homepage

--- frontend-vite/src/components/ProjectStatistics.jsx
+++ frontend-vite/src/components/ProjectStatistics.jsx
@@ -1,4 +1,52 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+// this data can be from api or other js files
+const data = [
+  {
+    name: "Jan-23",
+    client: 14,
+    lawyer: 6,
+    judge: 1,
+    cases: 2,
+  },
+  {
+    name: "Apr-23",
+    client: 18,
+    lawyer: 7,
+    judge: 3,
+    cases: 16,
+  },
+  {
+    name: "Jul-23",
+    client: 31,
+    lawyer: 15,
+    judge: 6,
+    cases: 18,
+  },
+  {
+    name: "Dec-23",
+    client: 57,
+    lawyer: 13,
+    judge: 17,
+    cases: 42
+  },
+  {
+    name: "Apr-24",
+    client: 65,
+    lawyer: 26,
+    judge: 19,
+    cases: 63,
+  },
+];
 
 const ProjectStatistics = () => {
   const [counters, setCounters] = useState({
@@ -71,9 +119,51 @@ const ProjectStatistics = () => {
 
         {/* RightSection >>> StaticticalChart*/}
         <div className="right-section w-[60%] border border-blue-500 rounded-lg flex justify-center items-center font-montserrat">
-          <div>
-            <h3>Chart Code</h3>
-          </div>
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart data={data}>
+              <CartesianGrid horizontal={false} vertical={false} />
+              <XAxis
+                dataKey="name" // the key-value pair should match in data array
+                angle={-35}
+                height={30}
+                tick={{ strokeWidth: 1.5, fontSize: 10, dy: 10, x: 0, y: 0 }}
+                padding={{ left: 20, right: 20 }}
+              />
+              <YAxis
+                type="number"
+                includeHidden={false}
+                axisLine={false}
+                padding={{ top: 10, bottom: 10 }}
+                tick={{ strokeWidth: 1.5, fontSize: 10 }}
+                width={30}
+              />
+              <Legend height={25} />
+              <Line
+                type="monotone"
+                strokeWidth={2.5}
+                name="client"
+                dataKey="client" // the key-value pair should match in data array
+                stroke="#4472C4"
+              />
+              <Line
+                type="monotone"
+                strokeWidth={2.5}
+                dataKey="lawyer" // the key-value pair should match in data array
+                stroke="#FFC000"
+              />
+              <Line
+                type="monotone"
+                strokeWidth={2.5}
+                dataKey="judge" // the key-value pair should match in data array
+                stroke="#A5A5A5"
+              /><Line
+                type="monotone"
+                strokeWidth={2.5}
+                dataKey="cases" // the key-value pair should match in data array
+                stroke="#ED7D31"
+              />
+            </LineChart>
+          </ResponsiveContainer>
         </div>
       </div>
     </div>
